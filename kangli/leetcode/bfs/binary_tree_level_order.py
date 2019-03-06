2ce8cbd9c3cf70b6c98317d6b5c2273ccf1af2a7
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-03-06 12:31:09 -0600

    rename level_order file

---
+++ kangli/leetcode/bfs/binary_tree_level_order.py
@@ -0,0 +1,29 @@
+#definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+from collections import deque
+class Solution:
+
+    def levelOrder(self, root):
+        if not root:
+            return []
+        queue, res = deque([root]), []
+
+        while queue:
+            cur_level, size = [], len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                cur_level.append(node.val)
+            res.append(cur_level)
+        return res
+
+
+
