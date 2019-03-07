3ca052bbf3d85661f13ce2981938d61cd7888cde
KunyiLiu <kl2912@columbia.edu>
2019-03-06 23:34:47 -0500

    double + single linked list

--- kunyi/data_structure/lru.py
+++ kunyi/data_structure/lru.py
@@ -63,3 +63,79 @@ class LRUCache:
         if len(self.data_set.keys()) > self.capacity:
             del self.data_set[self.tail.prev.key]
             self.deleteNode(self.tail.prev)
+
+ # single linked list 
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self._next = None
+
+class LRUCache:
+    """
+    @param: capacity: An integer
+    """
+
+    def __init__(self, capacity):
+        # head = dummy ->
+        # have KeyToPrev hash table to record the previous node
+        self.head = Node(0, 0)
+        self.tail = self.head
+        self.capacity = capacity
+        self.KeyToPrev = {}
+        
+    def moveToTail(self, prev):
+        # move the node the tail
+        # prev -> node -> next -> ... -> tail
+        # prev -> next -> ... -> tail -> node 
+        # prev -> next ->  ... -> node (tail)
+        node = prev._next 
+        if self.tail == node:
+            return 
+        prev._next = node._next
+        if node._next:
+            self.KeyToPrev[node._next.key] = prev 
+        node._next = None 
+        self.tail._next = node
+        self.KeyToPrev[node.key] = self.tail 
+        self.tail = node 
+        
+        
+    def popUpFront(self):
+        node = self.head._next
+        del self.KeyToPrev[node.key]
+        self.head._next = node._next 
+        self.KeyToPrev[self.head._next.key] = self.head
+       
+
+    """
+    @param: key: An integer
+    @return: An integer
+    """
+    def get(self, key):
+        # write your code here
+        if key not in self.KeyToPrev:
+            return -1 
+        prev = self.KeyToPrev[key]
+        self.moveToTail(prev)
+        # prev position has changed, so cannot use prev._next.value
+        return  self.KeyToPrev[key]._next.value 
+
+    """
+    @param: key: An integer
+    @param: value: An integer
+    @return: nothing
+    """
+    def set(self, key, value):
+        # write your code here
+        if key in self.KeyToPrev:
+            prev = self.KeyToPrev[key]
+            prev._next.value = value 
+            self.moveToTail(prev)
+        else: 
+            node = Node(key, value)
+            self.tail._next = node 
+            self.KeyToPrev[key] = self.tail 
+            self.tail = node
+            if len(self.KeyToPrev) > self.capacity:
+                self.popUpFront()
