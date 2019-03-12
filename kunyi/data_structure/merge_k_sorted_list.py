f2f9a8a8c13ff0db972a8077bb76059299775014
KunyiLiu <kl2912@columbia.edu>
2019-03-11 22:11:53 -0400

    Create merge_k_sorted_list.py

---
+++ kunyi/data_structure/merge_k_sorted_list.py
@@ -0,0 +1,45 @@
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param lists: a list of ListNode
+    @return: The head of one sorted list.
+    """
+    def mergeKLists(self, lists):
+        # method 1: from bottom to up mergeKLists
+        if len(lists) == 0 or lists is None:
+            return 
+        node = self.helper(lists, 0, len(lists) - 1)
+        return node
+        
+    def helper(self, lists, start, end):
+        if start == end:
+            return lists[start]
+        mid = start + (end - start) / 2
+        left_node = self.helper(lists, start, mid)
+        right_node = self.helper(lists, mid+1, end)
+        node = self.merge2(left_node, right_node)
+        return node 
+        
+    
+    def merge2(self, l1, l2):
+        dummy = ListNode(-1)
+        head = dummy
+        while l1 is not None and l2 is not None:
+            if l1.val > l2.val:
+                head.next = l2
+                head = l2
+                l2 = l2.next
+            else:
+                head.next = l1 
+                head = l1 
+                l1 = l1.next 
+                
+        head.next = l1 if l1 else l2 
+        return dummy.next 
