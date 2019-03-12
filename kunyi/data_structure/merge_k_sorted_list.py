afbc8ba7183d15b97803c4248f194e7fbb805e72
KunyiLiu <kl2912@columbia.edu>
2019-03-11 22:43:07 -0400

    dq or heapq(for k frequent problem)

--- kunyi/data_structure/merge_k_sorted_list.py
+++ kunyi/data_structure/merge_k_sorted_list.py
@@ -43,3 +43,38 @@ class Solution:
                 
         head.next = l1 if l1 else l2 
         return dummy.next 
+    
+# method 2: heapq
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+import heapq
+class Solution:
+    """
+    @param lists: a list of ListNode
+    @return: The head of one sorted list.
+    """
+    def mergeKLists(self, lists):
+        heap = []
+        for node in lists:
+            if node is not None:
+                self.heapq_push_node(heap, node)
+                
+        dummy = ListNode(-1)
+        head = dummy 
+        while len(heap) > 0:
+            _, target = heapq.heappop(heap)
+            head.next = target
+            head = head.next 
+            if target.next:
+                self.heapq_push_node(heap, target.next)
+            
+        return dummy.next 
+        
+    def heapq_push_node(self, heap, node):
+        heapq.heappush(heap, (node.val, node))
