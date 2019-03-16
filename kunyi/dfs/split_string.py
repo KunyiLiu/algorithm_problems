beeada76a43cbeb673c14e72e2f6f39bd233c290
KunyiLiu <kl2912@columbia.edu>
2019-03-16 12:43:40 -0500

    Update split_string.py

--- kunyi/dfs/split_string.py
+++ kunyi/dfs/split_string.py
@@ -47,3 +47,37 @@ class Solution:
                 path.append(s[:i+1])
                 self.dfs(result, path, s[i+1:])
                 path.pop()
+                
+ # method 3
+class Solution:
+    """
+    @param: : a string to be split
+    @return: all possible split string array
+    """
+
+    def splitString(self, s):
+        # write your code here
+        if s is None:
+            return []
+        if len(s) == 0:
+            return [[]]
+        self.result = []
+        self.s = s
+        self.dfs([], 0)
+        return self.result 
+        
+    def dfs(self, subset, ind):
+        if ind == len(self.s):
+            self.result.append(subset[:])
+            return 
+        
+        # subset.append(self.s[ind])
+        self.dfs(subset + [self.s[ind]], ind + 1)
+        # subset.pop()
+
+        if ind + 1 < len(self.s):
+            # subset.append(self.s[ind:(ind+2)])
+            self.dfs(subset + [self.s[ind:(ind+2)]], ind + 2)
+            # subset.pop()
+        
+        return
