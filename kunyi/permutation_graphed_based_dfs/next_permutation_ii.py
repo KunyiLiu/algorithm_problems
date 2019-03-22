7c4a6201f5e0ad678d9b111d44c0340d02a2ce74
KunyiLiu <kl2912@columbia.edu>
2019-03-21 23:29:05 -0400

    Create next_permutation_ii.py

---
+++ kunyi/permutation_graphed_based_dfs/next_permutation_ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    """
+    @param nums: An array of integers
+    @return: nothing
+    """
+    def nextPermutation(self, nums):
+        # write your code here
+        n = len(nums)
+        # corner case  
+        if n < 2:
+            return nums 
+        for i in range(n-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                break
+        # find the left ind of the descending sublit 
+        else:
+            self.inplace_reverse(nums, 0, n-1)
+            return nums
+            
+        for j in range(n-1, i, -1):
+            if nums[j] > nums[i]:
+                break 
+            
+        nums[i], nums[j] = nums[j], nums[i]
+        
+        self.inplace_reverse(nums, i+1, n-1)
+        
+        return nums 
+        
+    def inplace_reverse(self, nums, i, j):
+        while i <= j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1 
+            j -= 1 
