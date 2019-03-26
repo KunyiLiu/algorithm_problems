3812b2c2f7431ebfb850f5c3bd3e7bba0c85372b
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-03-25 19:02:38 -0500

    add a slightly different implementation during review

--- kangli/leetcode/backtracking/permutations.py
+++ kangli/leetcode/backtracking/permutations.py
@@ -12,3 +12,22 @@ class Solution:
         res = []
         dfs(nums, 0, [], res)
         return res
+
+#slight variation written during review
+
+class Solution(object):
+    def permute(self, nums):
+        res = []
+        self.build([], res, nums)
+        return res
+
+    def build(self, temp, res, nums):
+        if len(temp) == len(nums):
+            res.append(temp[:])
+            start = 0
+            return
+        for n in nums:
+            if n not in temp:
+                temp.append(n)
+                self.build(temp, res, nums)
+                temp.pop()
