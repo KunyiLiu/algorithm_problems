a5c1a228b9e445aa9552a4249aedd84808d8fb93
KunyiLiu <kl2912@columbia.edu>
2019-03-30 13:44:36 -0500

    Update binary_tree_inorder.py

--- kunyi/Tree/binary_tree_inorder.py
+++ kunyi/Tree/binary_tree_inorder.py
@@ -27,3 +27,43 @@ class Solution:
         self.helper(root.right)
         
         return 
+
+    
+# iteration
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+    def inorderTraversal(self, root):
+        # stack record the path of nodes that lead to node (not includes node)
+        result, stack = [], []
+        if root is None:
+            return result 
+        node = root 
+        while node:
+            stack.append(node)
+            node = node.left 
+        
+        while stack:
+            node = stack[-1]
+            result.append(node.val) 
+            if node.right:
+                node = node.right
+                while node:
+                    stack.append(node)
+                    node = node.left
+            else:
+                node = stack.pop()
+                while stack and stack[-1].right == node:
+                    node = stack.pop()
+                
+        return result 
