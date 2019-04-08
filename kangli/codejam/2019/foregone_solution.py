35a864d08dc1e4fedf2d19f0c122034b34a65afc
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-04-08 17:05:56 -0500

    final solution, passed all 3 test sets

--- kangli/codejam/2019/foregone_solution.py
+++ kangli/codejam/2019/foregone_solution.py
@@ -1,6 +1,36 @@
 
+def solve(N):
+    a, b = [], []
+    for n in N:
+        digits = generate_digits(n)
+        a.append(digits[0])
+        b.append(digits[1])
+    a, b = int("".join(str(x) for x in a)), int("".join(str(y) for y in b))
+    return a, b
+
+def generate_digits(n):
+    lookup = {
+        '0':['0', '0'],
+        '1':['0', '1'],
+        '2':['1', '1'],
+        '3':['1', '2'],
+        '4':['2', '2'],
+        '5':['2', '3'],
+        '6':['3', '3'],
+        '7':['2', '5'],
+        '8':['3', '5'],
+        '9':['3', '6']
+    }
+    return lookup[n]
+
+t = int(raw_input()) # read a line with a single integer
+for i in xrange(1, t + 1):
+    N = raw_input()
+    n, m = solve(N)
+    print "Case #{}: {} {}".format(i, n, m)
 
 
+'''
 def solve(N):
     a, b = [], []
     for n in N:
@@ -26,3 +56,4 @@ for i in xrange(1, t + 1):
     N = tests[i]
     n, m = solve(N)
     print "Case #{}: {} {}".format(i, n, m)
+'''
