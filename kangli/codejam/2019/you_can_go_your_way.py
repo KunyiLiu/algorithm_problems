9bf1e05fdd0fb9060e83866232ebb69bed9c5769
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-04-08 17:05:24 -0500

    update solutions

---
+++ kangli/codejam/2019/you_can_go_your_way.py
@@ -0,0 +1,15 @@
+def make_path(path):
+    my_path = []
+    for c in path:
+        if c == 'E':
+            my_path.append('S')
+        else:
+            my_path.append('E')
+    return "".join(x for x in my_path)
+
+t = int(raw_input()) # read a line with a single integer
+for i in xrange(1, t + 1):
+    n = int(raw_input())
+    path = raw_input()
+    my_path = make_path(path)
+    print("Case #{}: {}".format(i, my_path))
