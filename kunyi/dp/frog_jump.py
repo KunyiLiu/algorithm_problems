56526cbd5b5ad896f0de799094ba2e19e41c426a
KunyiLiu <kl2912@columbia.edu>
2019-04-18 21:50:59 -0500

    dp is a dict.dp[stone] set, record steps to stone.

---
+++ kunyi/dp/frog_jump.py
@@ -0,0 +1,32 @@
+class Solution:
+    """
+    @param stones: a list of stones' positions in sorted ascending order
+    @return: true if the frog is able to cross the river or false
+    """
+    def canCross(self, stones):
+        # dp {}
+        # dp[stone] set, record steps to stone.
+        # inference
+        # jump k - 1 to stone + k - 1:dp[stone + k - 1].add(k - 1)
+        # jump k to stone + k:dp[stone + k].add(k)
+        # jump k + 1 to stone + k + 1:dp[stone + k + 1].add(k + 1)
+        # see if len(dp[nth stone]) > 0 
+        dp = {}
+        for stone in stones:
+            dp[stone] = set([])
+            
+        dp[0].add(0)
+        for stone in stones:
+            for k in dp[stone]:
+                # forward
+                if k - 1 > 0 and stone + k - 1 in dp:
+                    dp[stone + k - 1].add(k-1)
+                    
+                if stone + k in dp:
+                    dp[stone + k].add(k)
+                    
+                if stone + k + 1 in dp:
+                    dp[stone + k + 1].add(k+1)
+                    
+                    
+        return len(dp[stones[-1]]) > 0
