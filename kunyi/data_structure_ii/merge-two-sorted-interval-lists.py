cef4d43a5ea44ce30fb027f9374ef8e0f106eafc
KunyiLiu <kl2912@columbia.edu>
2019-05-14 23:17:04 -0500

    combine 1. merge intervals 2. merge sorted arrays

--- kunyi/data_structure_ii/merge-two-sorted-interval-lists.py
+++ kunyi/data_structure_ii/merge-two-sorted-interval-lists.py
@@ -25,3 +25,41 @@ class Solution:
                 result[-1].end = max(result[-1].end, interval.end)
                 
         return result 
+
+    
+   #########  faster  #########
+class Solution:
+    """
+    @param list1: one of the given list
+    @param list2: another list
+    @return: the new sorted list of interval
+    """
+    def mergeTwoInterval(self, list1, list2):
+        # O(m + n)
+        result = []
+        i, j = 0, 0 
+        while i < len(list1) and j < len(list2):
+            if list1[i].start < list2[j].start:
+                self.push_back(result, list1[i])
+                i += 1 
+            else:
+                self.push_back(result, list2[j])
+                j += 1 
+                
+        while i < len(list1):
+            self.push_back(result, list1[i])
+            i += 1 
+            
+        while j < len(list2):
+            self.push_back(result, list2[j])
+            j += 1
+            
+        return result 
+        
+    def push_back(self, result, element):
+        if len(result) == 0 or result[-1].end < element.start:
+            result.append(element)
+        else:
+            result[-1].end = max(result[-1].end, element.end)
+            
+        return 
