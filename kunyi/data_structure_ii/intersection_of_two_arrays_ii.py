a7b4ca40a45eae3f54312ed0a1e0ea6a52dcf574
KunyiLiu <kl2912@columbia.edu>
2019-05-15 23:38:17 -0400

    1. counter hash table 2. two pointers

--- kunyi/data_structure_ii/intersection_of_two_arrays_ii.py
+++ kunyi/data_structure_ii/intersection_of_two_arrays_ii.py
@@ -17,3 +17,27 @@ class Solution:
                 counter[num] -= 1 
                 
         return result
+    
+class Solution:
+    """
+    @param nums1: an integer array
+    @param nums2: an integer array
+    @return: an integer array
+    """
+    def intersection(self, nums1, nums2):
+        # sort + two pointers 
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        p1, p2 = 0, 0 
+        result = []
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] < nums2[p2]:
+                p1 += 1 
+            elif nums1[p1] > nums2[p2]:
+                p2 += 1 
+            else:
+                result.append(nums1[p1])
+                p1 += 1 
+                p2 += 1 
+                
+        return result 
