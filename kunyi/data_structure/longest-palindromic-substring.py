4a9837f3f36d425a815904023fab61b93b68f512
KunyiLiu <kl2912@columbia.edu>
2019-06-12 00:16:58 -0400

    str

--- kunyi/data_structure/longest-palindromic-substring.py
+++ kunyi/data_structure/longest-palindromic-substring.py
@@ -22,3 +22,68 @@ class Solution:
             i += 1 
             j -= 1 
         return i >= j
+
+############# O(n^2) Enum from mid point ########
+
+class Solution:
+    """
+    @param s: input string
+    @return: the longest palindromic substring
+    """
+    def longestPalindrome(self, s):
+        # Time complexity O(n^2)
+        # expand the substring from the mid point 
+        if not s:
+            return ''
+            
+        self.start, self.longest = 0, 0 
+        for mid in range(len(s)):
+            # update self.start and self.longest
+            self.helper(s, mid, mid)
+            self.helper(s, mid, mid + 1)
+            
+        return s[self.start:(self.start + self.longest)]
+        
+    def helper(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1 
+            right += 1 
+        local = right - 1 - (left + 1) + 1
+        if self.longest < local:
+            self.longest = local
+            self.start = left + 1
+
+############# O(n^2) dp #############
+class Solution:
+    """
+    @param s: input string
+    @return: the longest palindromic substring
+    """
+    def longestPalindrome(self, s):
+        # dp[i][j] - is_pal substring between i to j  
+        # dp[i][i] means the single char from string, True
+        # dp[i][j] = (dp[i + 1][j-1] and s[i] == s[j]
+        
+        if not s:
+            return ''
+        
+        len_s = len(s)
+        dp = [[False] * len_s for i in range(len_s)]
+        start, longest = 0, 1
+        for i in range(len_s):
+            dp[i][i] = True 
+            
+        for length in range(2, len_s + 1):
+            for i in range(len_s - length + 1):
+                j = i + length - 1 
+                # start from 0, 1, 
+                if i + 1 < j:
+                    dp[i][j] = dp[i+1][j-1] and s[i] == s[j]
+                else:
+                    dp[i][j] = s[i] == s[j]
+                
+                if dp[i][j] and length > longest:
+                    start = i 
+                    longest = length
+        # start + longest - 1 - start + 1 = longest
+        return s[start: (start + longest)]    
