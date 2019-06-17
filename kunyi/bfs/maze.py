489b7e2201d153880682764c0067f3de69c703af
KunyiLiu <kl2912@columbia.edu>
2019-06-16 23:59:28 -0500

    Update maze.py

--- kunyi/bfs/maze.py
+++ kunyi/bfs/maze.py
@@ -37,3 +37,42 @@ class Solution:
             if (final_x != x or final_y != y) and (final_x, final_y) not in visited:
                 queue.put([final_x, final_y])
                 visited.add((final_x, final_y))
+
+ #### method 
+class Solution:
+    """
+    @param maze: the maze
+    @param start: the start
+    @param destination: the destination
+    @return: whether the ball could stop at the destination
+    """
+    def hasPath(self, maze, start, destination):
+        # write your code here
+        if maze is None or len(maze) == 0:
+            return False 
+        
+        m, n = len(maze), len(maze[0])
+        queue = [start]
+        visited = [[False] * n for i in range(m)]
+        delta_X = [1, -1, 0, 0]
+        delta_Y = [0, 0, 1, -1]
+        while len(queue) > 0:
+            x, y = queue.pop(0)
+            # not level order, can be put here
+            # can also modify maze[x][y] = 2
+            visited[x][y] = True 
+            if x == destination[0] and y == destination[1]:
+                return True 
+            for i in range(4):
+                new_x, new_y = x + delta_X[i], y + delta_Y[i]
+                # rolling
+                while 0 <= new_x < m and 0 <= new_y < n and maze[new_x][new_y] != 1:
+                    new_x += delta_X[i]
+                    new_y += delta_Y[i]
+                    
+                new_x -= delta_X[i]
+                new_y -= delta_Y[i]
+                if visited[new_x][new_y] is False:
+                    queue.append([new_x, new_y])
+                    
+        return False
