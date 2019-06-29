d91e616dd601e5ca60aea149bf886d6620a84e1b
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-06-29 17:10:51 -0500

    add solution using defaultdict to group anagrams

--- kangli/leetcode/hash_table/group_anagrams.py
+++ kangli/leetcode/hash_table/group_anagrams.py
@@ -8,3 +8,29 @@ class Solution(object):
         for g in groups:
             res.append(groups[g])
         return res
+
+
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs):
+        d = defaultdict(list)
+        for w in strs:
+            k = ''.join(sorted(w))
+            d[k].append(w)
+        res = []
+        for v in d.values():
+            res.append(v)
+        return res
+
+
+'''
+Related Topics: Hash Table, String
+Similar Questions: Valid Anagram, Group Shifted Strings
+
+Success
+Details 
+Runtime: 108 ms, faster than 95.98% of Python3 online submissions for Group Anagrams.
+Memory Usage: 16.1 MB, less than 53.87% of Python3 online submissions for Group Anagrams.
+Next challenges:
+Group Shifted Strings
+'''
