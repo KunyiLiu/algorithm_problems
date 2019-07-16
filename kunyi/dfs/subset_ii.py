fbea30e1aadde198093f45876fcbc93319af3341
KunyiLiu <kl2912@columbia.edu>
2019-07-15 22:13:50 -0500

    Update subset_ii.py

--- kunyi/dfs/subset_ii.py
+++ kunyi/dfs/subset_ii.py
@@ -26,3 +26,27 @@ class Solution:
             subset.pop()
             
         return 
+
+##### 
+
+class Solution:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsetsWithDup(self, nums):
+        # introduce start_ind, dont look back 
+        result = []
+        # point 1. sorted 
+        nums = sorted(nums)
+        self.helper(nums, result, [], 0)
+        return result 
+        
+    def helper(self, nums, result, subset, start_ind):
+        result.append(subset[:])
+        for i in range(start_ind, len(nums)):
+            if i > start_ind and nums[i] == nums[i-1]:
+                continue
+            subset.append(nums[i])
+            self.helper(nums, result, subset, i + 1)
+            subset.pop()
