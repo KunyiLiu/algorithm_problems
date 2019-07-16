9d0a76d78a1005c8e85249a40097b9791aea0698
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-07-15 20:52:32 -0500

    add solution using visited set

--- kangli/leetcode/backtracking/permutations.py
+++ kangli/leetcode/backtracking/permutations.py
@@ -31,3 +31,33 @@ class Solution(object):
                 temp.append(n)
                 self.build(temp, res, nums)
                 temp.pop()
+                
+class Solution:
+    def permute(self, nums):
+        res = []
+        self.dfs(nums, [], res, set())
+        return res 
+    
+    def dfs(self, nums, subset, res, visited):
+      
+        if len(subset) == len(nums):
+            res.append(subset[:])
+            return
+        for n in nums:
+            if n not in visited:
+                subset.append(n)
+                visited.add(n)
+                self.dfs(nums, subset, res, visited)
+                subset.pop()
+                visited.remove(n)
+'''
+Success
+Details 
+Runtime: 44 ms, faster than 97.14% of Python3 online submissions for Permutations.
+Memory Usage: 13.3 MB, less than 37.02% of Python3 online submissions for Permutations.
+Next challenges:
+Next Permutation
+Permutations II
+Permutation Sequence
+Combinations
+'''
