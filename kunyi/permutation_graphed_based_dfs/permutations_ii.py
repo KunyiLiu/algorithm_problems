2bc35955b23d6fc3a1130715d4e9dec0d1570551
KunyiLiu <kl2912@columbia.edu>
2019-07-15 21:36:44 -0500

    Update permutations_ii.py

--- kunyi/permutation_graphed_based_dfs/permutations_ii.py
+++ kunyi/permutation_graphed_based_dfs/permutations_ii.py
@@ -26,3 +26,39 @@ class Solution:
             sub1 = nums[i]
             sub2 = nums[:i] + nums[(i+1):]
             self.helper(sub2, tmp + [sub1])
+
+ ######### 
+class Solution:
+    """
+    @param: :  A list of integers
+    @return: A list of unique permutations
+    """
+
+    def permuteUnique(self, nums):
+        # since we have duplicate numbers, 
+        # we cannot use set to check if the num has been put in subset
+        # T(n) = O(n**2)
+        # unique: i > 0, nums[i] == nums[i-1], sorted
+        
+        result = []
+        if nums is None:
+            return result
+        # point 1. sort nums
+        nums = sorted(nums)
+        self.helper(nums, result, [])
+        return result 
+        
+    def helper(self, nums, result, subset):
+        if len(nums) == 0:
+            # point 2. deep copy
+            result.append(subset[:])
+            return 
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+            subset.append(nums[i])
+            self.helper(nums[:i] + nums[(i+1):], result, subset)
+            # point 3. backtrack
+            subset.pop()
+        
