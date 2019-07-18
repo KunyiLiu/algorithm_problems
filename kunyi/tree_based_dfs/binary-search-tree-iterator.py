e9b069d07408a29d6cc7b1d3b3d3702d6fe58db1
KunyiLiu <kl2912@columbia.edu>
2019-07-18 02:31:08 -0400

    current point; linked list

--- kunyi/tree_based_dfs/binary-search-tree-iterator.py
+++ kunyi/tree_based_dfs/binary-search-tree-iterator.py
@@ -44,3 +44,49 @@ class BSTIterator:
         nxt = node 
         self.cur = node.right 
         return nxt
+
+############### To Linked List, Time O(1), Space O(1) ##############
+class BSTIterator:
+    """
+    @param: root: The root of binary tree.
+    """
+    def build_linked_list(self, root):
+        if root is None:
+            return 
+        
+        root_left = self.build_linked_list(root.left)
+        root_right = self.build_linked_list(root.right)
+        if root_left:
+            node = root_left 
+            while node.right is not None:
+                node = node.right 
+            node.right = root 
+            root.left = None
+            root.right = root_right 
+            return root_left
+        else:
+            root.right = root_right
+            root.left = None
+            return root
+    
+    def __init__(self, root):
+        # mehod 2. convert root to linked list starting with root 
+        root_top = self.build_linked_list(root)
+        self.cur = root_top
+
+    """
+    @return: True if there has next node, or false
+    """
+    def hasNext(self, ):
+        # write your code here
+        return self.cur is not None
+        
+
+    """
+    @return: return next node
+    """
+    def next(self, ):
+        # write your code here
+        node = self.cur 
+        self.cur = node.right
+        return node
