46b05eb205dbaaf23081db08cbf8968ca64910b7
KunyiLiu <kl2912@columbia.edu>
2019-07-20 18:04:42 -0500

    DQ, ResultType

--- kunyi/tree_based_dfs/lower_common_ancestor_binary_tree.py
+++ kunyi/tree_based_dfs/lower_common_ancestor_binary_tree.py
@@ -88,3 +88,42 @@ class Solution:
         if root == B:
             result.isB = True
         return result
+    
+####### customized ########
+class ResultType:
+    def __init__(self):
+        self.is_a = False
+        self.is_b = False
+        self.lca = None
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: A: A TreeNode in a Binary.
+    @param: B: A TreeNode in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """
+    def lowestCommonAncestor(self, root, A, B):
+        # botton up, DQ 
+        result = self.dfs(root, A, B)
+        return result.lca
+        
+    def dfs(self, root, A, B):
+        if root is None:
+            return ResultType()
+            
+        left_result = self.dfs(root.left, A, B)
+        right_result = self.dfs(root.right, A, B)
+        # iteration - break;
+        # recursion: dq - keep the same resultType; traverse - only change when requirements met 
+        if left_result.is_a and left_result.is_b:
+            return left_result
+        if right_result.is_a and right_result.is_b:
+            return right_result
+        result = ResultType()
+        result.is_a = left_result.is_a or right_result.is_a or root == A 
+        result.is_b = left_result.is_b or right_result.is_b or root == B 
+        if result.is_a and result.is_b:
+            result.lca = root
+        return result 
+
