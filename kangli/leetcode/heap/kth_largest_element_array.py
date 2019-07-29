1aeb109a2486dfbec89439512072b10c5700079c
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-07-29 17:25:28 -0500

    Create kth_largest_element_array.py

---
+++ kangli/leetcode/heap/kth_largest_element_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums)-k]
+        
+
+'''
+Success
+Details 
+Runtime: 72 ms, faster than 93.95% of Python3 online submissions for Kth Largest Element in an Array.
+Memory Usage: 14.8 MB, less than 7.39% of Python3 online submissions for Kth Largest Element in an Array.
+Next challenges: Wiggle Sort II, Top K Frequent Elements, Third Maximum Number, Kth Largest Element in a Stream
+
+Related Topics: Divide and Conquer, Heap
+'''
