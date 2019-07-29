a6a986f080406521e9a8c60e758a50437a00de2b
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-07-29 17:27:11 -0500

    add similar questions list

--- kangli/leetcode/heap/kth_largest_element_array.py
+++ kangli/leetcode/heap/kth_largest_element_array.py
@@ -12,4 +12,6 @@ Memory Usage: 14.8 MB, less than 7.39% of Python3 online submissions for Kth Lar
 Next challenges: Wiggle Sort II, Top K Frequent Elements, Third Maximum Number, Kth Largest Element in a Stream
 
 Related Topics: Divide and Conquer, Heap
+Similar Questions: Medium, Top K Frequent Elements, Third Maximum Number, Kth Largest Element in a Stream, 
+K Closest Points to Origin
 '''
