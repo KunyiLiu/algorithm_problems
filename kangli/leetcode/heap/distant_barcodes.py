4a7dc84a5138c2129823aee3f05ccee3211ad8ff
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-07-29 23:06:19 -0500

    add submission output

--- kangli/leetcode/heap/distant_barcodes.py
+++ kangli/leetcode/heap/distant_barcodes.py
@@ -20,3 +20,12 @@ class Solution:
                 num, count = modes.pop(0)
         
         return res
+ 
+
+'''
+Success
+Details 
+Runtime: 588 ms, faster than 29.55% of Python3 online submissions for Distant Barcodes.
+Memory Usage: 15.9 MB, less than 100.00% of Python3 online submissions for Distant Barcodes.
+Next challenges: Meeting Rooms II, Best Meeting Point, Find K Pairs with Smallest Sums
+'''
