cae028ba3f7257466fa6d7c23162a8e4f406fb79
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-07-30 00:48:14 -0500

    reviewed longest common prefix

--- kangli/leetcode/sliding_window/longest_common_prefix.py
+++ kangli/leetcode/sliding_window/longest_common_prefix.py
@@ -1,4 +1,39 @@
 class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        for i in range(len(strs)):
+            if i == 0:
+                pref = self.get_prefix(strs[i], strs[i+1])
+            else:
+                pref = self.get_prefix(pref, strs[i])
+        return pref
+            
+    def get_prefix(self, s1, s2):
+        pref = []
+        if not s1 or not s2:
+            return ""
+        for i in range(min(len(s1), len(s2))):
+            if s1[i] == s2[i]:
+                pref.append(s1[i])
+            else:
+                break
+        return "".join(pref)
+            
+        
+'''
+Success
+Details 
+Runtime: 48 ms, faster than 21.67% of Python3 online submissions for Longest Common Prefix.
+Memory Usage: 14 MB, less than 5.41% of Python3 online submissions for Longest Common Prefix.
+Next challenges: Longest Substring with At Most Two Distinct Characters, Count Different Palindromic Subsequences, 
+Find And Replace in String
+'''
+
+
+class Solution:
     def longestCommonPrefix(self, strs):
         if not strs:
             return ""
