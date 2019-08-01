dde50733ca4fde4786e5299ae1484263db8ab579
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-08-01 14:44:14 -0500

    reviewed word break

--- kangli/leetcode/dynamic_programming/word_break.py
+++ kangli/leetcode/dynamic_programming/word_break.py
@@ -1,5 +1,26 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False]*(len(s)+1)
+        dp[0] = True
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                
+                print(j, s[i:j+1])
+                if dp[i] and s[i:j+1] in wordDict:
+                    dp[j+1] = True
+        return dp[-1]
+    
+
+'''
+Success
+Details 
+Runtime: 100 ms, faster than 5.70% of Python3 online submissions for Word Break.
+Memory Usage: 14.2 MB, less than 5.33% of Python3 online submissions for Word Break.
+Next challenges: Word Break II
+'''
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         arr = [False for _ in range(len(s))]
         for i in range(len(s)):
             for w in wordDict:
