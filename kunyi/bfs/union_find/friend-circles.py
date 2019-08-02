60c1e6770a3b432b36d32dce7ccfa76653eb46f6
KunyiLiu <kl2912@columbia.edu>
2019-08-01 22:25:57 -0500

    UF / DFS

--- kunyi/bfs/union_find/friend-circles.py
+++ kunyi/bfs/union_find/friend-circles.py
@@ -46,3 +46,32 @@ class Solution:
             
         return len(set(parent))
         
+######### DFS ############
+"""
+Time complexity : O(n^2).
+
+Space complexity : O(n)
+"""
+class Solution:
+    def dfs(self, M, visited, ind):
+        # make the value of ind, which are connected to ind, to 1 
+        n = len(M)
+        for j in range(n):
+            if visited[j] is False and M[ind][j] == 1:
+                visited[j] = True 
+                self.dfs(M, visited, j)
+        return 
+
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        if M is None or len(M) == 0 or len(M[0]) == 0:
+            return 0
+        
+        n = len(M)
+        visited = [False] * n 
+        count = 0
+        for i in range(n):
+            if visited[i] is False:
+                self.dfs(M, visited, i)
+                count += 1 
+        
+        return count
