26dc324c009981a455cf62138d34d891a6cbfcb0
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-08-02 15:59:50 -0500

    add a cleaner solution

--- kangli/leetcode/two_pointers/squares_of_a_sorted_array.py
+++ kangli/leetcode/two_pointers/squares_of_a_sorted_array.py
@@ -1,4 +1,33 @@
 class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        res = [None]*len(A)
+        i, j = 0, len(A)-1
+        k = len(A)-1
+        while i <= j:
+            if abs(A[i]) >= A[j]:
+                res[k] = A[i]**2
+                i += 1 
+                k -= 1
+            else:
+                res[k] = A[j]**2
+                j -= 1
+                k -= 1
+        return res
+        
+
+'''
+Success
+Details 
+Runtime: 268 ms, faster than 52.97% of Python3 online submissions for Squares of a Sorted Array.
+Memory Usage: 15.7 MB, less than 5.10% of Python3 online submissions for Squares of a Sorted Array.
+Next challenges: Sort Transformed Array
+
+Related Topics: Array, Two Pointers
+Similar Questions: Merge Sorted Array, Sort Transformed Array
+'''
+
+
+class Solution:
     def sortedSquares(self, A):
         i, j = 0, len(A) - 1
         res = [0] * len(A)
