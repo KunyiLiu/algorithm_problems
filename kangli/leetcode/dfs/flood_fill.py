ed93ce368a00573969f8f29ec2d4988e7585c09e
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2019-08-05 23:23:03 -0500

    reviewed flood fill

--- kangli/leetcode/dfs/flood_fill.py
+++ kangli/leetcode/dfs/flood_fill.py
@@ -1,3 +1,34 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+     
+        def dfs(image, sr, sc, startColor, newColor):
+            if (sr, sc) not in visited and image[sr][sc] == startColor:
+                visited.add((sr, sc))
+                image[sr][sc] = newColor
+                for d in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    nr, nc = sr+d[0], sc+d[1]
+                    if 0 <= nr < len(image) and 0 <= nc < len(image[0]) and (nr, nc) not in visited and image[nr][nc] == startColor:
+                        dfs(image, nr, nc, startColor, newColor)
+                     
+        visited = set()
+        startColor = image[sr][sc]
+        dfs(image, sr, sc, startColor, newColor)
+        return image
+    
+    
+'''
+Success
+Details 
+Runtime: 92 ms, faster than 55.34% of Python3 online submissions for Flood Fill.
+Memory Usage: 14 MB, less than 7.32% of Python3 online submissions for Flood Fill.
+Next challenges: Island Perimeter
+
+Related Topics: Depth-first Search
+Similar Questions: Island Perimeter
+Hint: Write a recursive function that paints the pixel if it's the correct color, then recurses on neighboring pixels.
+'''
+
+
 # 733 Given 2d array, starting coordinates, and new Color, color all cells 4-directionally connected to starting cell with newColor.
 #Companies: Amazon(7), Google(3), Snapchat(2), Facebook(2), Zillow(2), Bloomberg(2), Uber.
 # related topics Depth-first Search, Similar Questions: Island Perimeter.
