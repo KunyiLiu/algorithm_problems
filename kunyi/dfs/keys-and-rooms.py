3e9247d4cde1a4af1d0c04877022d0a76f1870b4
KunyiLiu <kl2912@columbia.edu>
2019-08-09 14:15:25 -0500

    Update keys-and-rooms.py

--- kunyi/dfs/keys-and-rooms.py
+++ kunyi/dfs/keys-and-rooms.py
@@ -18,3 +18,25 @@ class Solution:
                     queue.append(ind)
                     
         return len(visited) == len(rooms)
+
+    
+####### DFS top down traverse ########
+class Solution:
+    """
+    @param rooms: a list of keys rooms[i]
+    @return: can you enter every room
+    """
+    def canVisitAllRooms(self, rooms):
+        # Write your code here
+        visited = set([0])
+        self.dfs(0, rooms, visited)
+        return len(visited) == len(rooms)
+        
+    def dfs(self, start, rooms, visited):
+        for ind in rooms[start]:
+            if ind in visited:
+                continue
+            visited.add(ind)
+            self.dfs(ind, rooms, visited)
+            
+        return 
