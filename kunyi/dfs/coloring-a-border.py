ba90355560dfce8fd7be1e0fc4924bae38a5a324
KunyiLiu <kl2912@columbia.edu>
2019-08-14 02:40:43 -0400

    Update coloring-a-border.py

--- kunyi/dfs/coloring-a-border.py
+++ kunyi/dfs/coloring-a-border.py
@@ -27,3 +27,40 @@ class Solution:
         for x, y in border:
             grid[x][y] = color
         return grid
+
+########### DFS #########
+class Solution:
+    def colorBorder(self, grid: List[List[int]], r0: int, c0: int, color: int) -> List[List[int]]:
+        """
+        set up a dfs helper function,
+        start from (r0, c0), find and color all the border nodes 
+        """
+        if grid is None or len(grid) == 0:
+            return grid 
+        
+        m, n = len(grid), len(grid[0])
+        seen = set()
+        def dfs(x, y, seen):
+            # will finally go to the first row, col, break this road
+            if (x, y) in seen:
+                return False  
+
+            # out of grid or not in the component
+            if not(0<= x < m and 0<= y < n and grid[x][y] == grid[r0][c0]):
+                return True
+            
+            # location
+            seen.add((x,y))
+            is_left = dfs(x-1, y, seen)
+            is_right = dfs(x+1, y, seen)
+            is_up = dfs(x, y-1, seen)
+            is_down = dfs(x, y+1, seen)
+            if any([is_left, is_right, is_up, is_down]):
+                grid[x][y] = color 
+                
+            return False 
+        
+        dfs(r0, c0, seen)
+        return grid
+            
+        
