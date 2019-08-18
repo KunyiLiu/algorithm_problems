9137b590eaf1849d6ac78cd0d76ad087a39fac57
KunyiLiu <kl2912@columbia.edu>
2019-08-18 12:56:58 -0500

    Update island-perimeter.py

--- kunyi/data_structure/hash_table/island-perimeter.py
+++ kunyi/data_structure/hash_table/island-perimeter.py
@@ -17,3 +17,24 @@ class Solution:
                         ans -= 2 
                     
         return ans
+    
+    
+##### BFS #####
+class Solution:
+    """
+    @param grid: a 2D array
+    @return: the perimeter of the island
+    """
+    def islandPerimeter(self, grid):
+        # Write your code here
+        ans = 0 
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    for delta_x, delta_y in [(-1,0), (1,0), (0, -1), (0, 1)]:
+                        new_i, new_j = i + delta_x, j + delta_y
+                        if not(0<= new_i < m and 0 <= new_j < n) or grid[new_i][new_j] == 0:
+                            ans += 1 
+                            
+        return ans
