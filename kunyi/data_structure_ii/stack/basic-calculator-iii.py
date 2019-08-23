3733f28eb2f972654230d46f76973ceadb6d2631
KunyiLiu <kl2912@columbia.edu>
2019-08-23 06:33:33 -0500

    Create basic-calculator-iii.py

---
+++ kunyi/data_structure_ii/stack/basic-calculator-iii.py
@@ -0,0 +1,44 @@
+class Solution:
+    """
+    @param s: the expression string
+    @return: the answer
+    """
+    def calculate(self, s):
+        # Write your code here
+        self.ind = 0
+        return self.helper(s)
+        
+    def helper(self, s):
+        stack, sign = [], '+'
+        s = s.strip()
+        while self.ind < len(s) and sign != ')':
+            if s[self.ind] == ' ':
+                self.ind += 1 
+                continue
+            
+            # generate num 
+            if s[self.ind] == '(':
+                self.ind += 1
+                num = self.helper(s)
+            else:
+                num = 0 
+                while self.ind < len(s) and s[self.ind].isdigit():
+                    num = num * 10 + int(s[self.ind])
+                    self.ind += 1
+            
+            if sign == '+':
+                stack.append(num)
+            elif sign == '-':
+                stack.append(-num)
+            elif sign == '*':
+                stack[-1] *= num
+            elif sign == '/':
+                stack[-1] /= num
+                
+            if self.ind >= len(s):
+                break
+
+            sign = s[self.ind] 
+            self.ind += 1 
+            
+        return sum(stack)
