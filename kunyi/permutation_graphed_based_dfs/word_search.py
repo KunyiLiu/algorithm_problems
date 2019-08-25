068c469d29066c74a06e2e5a0ceded9d43b576c7
KunyiLiu <kl2912@columbia.edu>
2019-08-24 22:01:38 -0500

    Update word_search.py

--- kunyi/permutation_graphed_based_dfs/word_search.py
+++ kunyi/permutation_graphed_based_dfs/word_search.py
@@ -31,3 +31,47 @@ class Solution:
         exist = self.ifexist(board, word, start + 1, visited, i+1, j) or self.ifexist(board, word, start + 1, visited, i-1, j) or self.ifexist(board, word, start + 1, visited, i, j + 1) or self.ifexist(board, word, start + 1, visited, i, j - 1)
         visited[i][j] = False # backtrack
         return exist
+
+ ####### similar to color board #########
+class Solution:
+    """
+    @param board: A list of lists of character
+    @param word: A string
+    @return: A boolean
+    """
+    def exist(self, board, word):
+        # cannot use BFS - some of the pos might have been taken by other paths 
+        # DFS - dfs(start_point, word), return boolean, if we can reach to the end if we start from the point 
+        if board is None or len(board) == 0 or len(board[0]) == 0:
+            return False 
+            
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    visited = [[False] * n for _ in range(m)]
+                    result = self.dfs(i, j, board, word, visited)
+                    if result:
+                        return True 
+                        
+        return False 
+        
+    def dfs(self, x, y, board, word, visited):
+        m, n = len(board), len(board[0])
+        if len(word) == 0:
+            return True 
+        
+        if not(0 <= x < m and 0 <= y < n and visited[x][y] is False and board[x][y] == word[0]):
+            return False 
+        
+        visited[x][y] = True 
+        is_left = self.dfs(x-1, y, board, word[1:], visited)
+        is_right = self.dfs(x+1, y, board, word[1:], visited)
+        is_up = self.dfs(x, y-1, board, word[1:], visited)
+        is_down = self.dfs(x, y+1, board, word[1:], visited)
+        if not any([is_left, is_right, is_up, is_down]):
+            # backtracking
+            visited[x][y] = False 
+            return False 
+            
+        return True
