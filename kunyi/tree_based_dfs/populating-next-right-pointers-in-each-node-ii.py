c151a1de8388d4df1d70dc5f4aded42e454c200f
KunyiLiu <kl2912@columbia.edu>
2019-08-31 18:08:14 -0500

    Create populating-next-right-pointers-in-each-node-ii.py

---
+++ kunyi/tree_based_dfs/populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,66 @@
+# reset cur and dummy
+# Time O(N), space O(1)
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        cur = dummy = Node()
+        node = root
+        while node:
+            if node.left:
+                cur.next = node.left
+                cur = node.left
+            if node.right:
+                cur.next = node.right
+                cur = node.right
+            node = node.next
+            if not node:
+                node = dummy.next
+                dummy.next = None # This is very important!!!
+                cur = dummy # This is very important!!!
+        return root
+        
+### recursive - find first node continuously ####
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        # set first_node, and cur 
+        # find the first_node
+        # first_node -> next -> first_node 
+        if root is None:
+            return 
+        
+        first_node, cur = root, None
+        while first_node:
+            cur = first_node 
+            first_node = self.construct_next_layer(cur, None)
+            
+        return root 
+    
+    def construct_next_layer(self, cur, visited):
+        # if cur is None:
+        #     return None
+        node = None 
+        while cur:
+            if cur.left and cur.left != visited:
+                node = cur.left
+                break
+            if cur.right and cur.right != visited:
+                node = cur.right
+                cur = cur.next
+                break
+            cur = cur.next 
+        ## ERROR: node judgement
+        if node:
+            node.next = self.construct_next_layer(cur, node)
+        return node 
