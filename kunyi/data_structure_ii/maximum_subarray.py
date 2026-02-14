f1963f5d57ba5b9680dde2fc48777e089e7ad310
Kunyi Liu <kl2912@columbia.edu>
2026-02-13 22:05:22 -0800

    Refactor maxSubArray method for clarity and efficiency

--- kunyi/data_structure_ii/maximum_subarray.py
+++ kunyi/data_structure_ii/maximum_subarray.py
@@ -1,22 +1,37 @@
 class Solution:
-    """
-    @param nums: A list of integers
-    @return: A integer indicate the sum of max subarray
-    """
-    def maxSubArray(self, nums):
-        # prefix sum + greedy 
-        # max{aj - ai}, and j > i
-        import sys
-        prefix_sums = [0]
-        tmp_sum = 0 
-        for i in range(len(nums)):
-            tmp_sum += nums[i]
-            prefix_sums.append(tmp_sum)
+    def maxSubArray(self, nums: List[int]) -> int:
+        # basically is calculating max(prefix_sum[i] - prefix_sum[j])
+        n = len(nums)
+        prefix_sum = [0]
+        for i in range(n):
+            prefix_sum.append(prefix_sum[-1] + nums[i])
+
+
+        max_subarray_sum = nums[0]
+        min_prefix_sum = prefix_sum[0]
+        for i in range(1, n + 1):
+            max_subarray_sum = max(max_subarray_sum, prefix_sum[i] - min_prefix_sum)
+            min_prefix_sum = min(min_prefix_sum, prefix_sum[i])
+
+        return max_subarray_sum
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane’s Algorithm, when the running sum is negative, reset it and 
+        # start a fresh subarray from the next element
+        # Time complexity: O(n), Space Complexity: O(1)
+        n = len(nums)
+        if n < 1:
+            return 
+
+        cur_sum, max_sum = 0, nums[0]
+        for num in nums:
+            if cur_sum < 0:
+                cur_sum = 0
             
-        max_res = - sys.maxsize 
-        min_prefix = prefix_sums[0]
-        for i in range(1, len(nums) + 1):
-            max_res = max(max_res, prefix_sums[i] - min_prefix)
-            min_prefix = min(min_prefix, prefix_sums[i])
-            
-        return max_res
+            cur_sum += num
+            max_sum = max(max_sum, cur_sum)
+
+        return max_sum
+
+        
