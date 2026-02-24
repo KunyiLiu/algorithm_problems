07180dc313c752121d975402061d6298bf4bbc6f
Kunyi Liu <kl2912@columbia.edu>
2026-02-23 22:01:29 -0800

    Add isNStraightHand method for hand grouping
    Implement greedy algorithm to check if hand can be grouped into straight hands of specified size.

--- kunyi/dp/greedy/Hand_of_Straights.py
+++ kunyi/dp/greedy/Hand_of_Straights.py
@@ -24,4 +24,28 @@ class Solution:
 
         return True
 
+###### greedy O(n)
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # greedy + stack -> car fleet
+        # so there is left card after rearraingment?
+        n = len(hand)
+        if n % groupSize != 0:
+            return False
+
+        count = Counter(hand)
+        for num in hand:
+            if count[num] == 0: continue # Skip if we already used this card
+
+            start = num
+            # find the smallest card, set it as start
+            while count[start - 1]:
+                start -= 1
+            while count[start]:
+                for i in range(start, start + groupSize):
+                    if not count[i]:
+                        return False
+                    count[i] -= 1
+
+        return True
         
