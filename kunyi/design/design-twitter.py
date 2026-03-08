cc10cae53f8a1d9b8df684ca9a5dc088a43d7cbd
Kunyi Liu <kl2912@columbia.edu>
2026-03-07 21:21:54 -0800

    Refactor Twitter class and methods for functionality

--- kunyi/design/design-twitter.py
+++ kunyi/design/design-twitter.py
@@ -1,82 +1,105 @@
-'''
-Definition of Tweet:
-class Tweet:
-    @classmethod
-    def create(cls, user_id, tweet_text):
-         # This will create a new tweet object,
-         # and auto fill id
-'''
+#####  min heap #
 
-class MiniTwitter:
-    
+class Twitter:
+    import heapq
     def __init__(self):
-        # do intialization if necessary
-        self.follow_dict = {}
-        self.time = 0 
-        # {uid: [(time, message), ]
-        self.message_dict = {}
+        # Space: #user * #tweets + #user * #user
+        # increase monotonically
+        self.orderId = 0
+        # userId: hashset(), then remove O(1)
+        self.followed_users = {}
+        # userId: [(orderId, tweetId)]
+        self.user_tweets = {}
 
-    """
-    @param: user_id: An integer
-    @param: tweet_text: a string
-    @return: a tweet
-    """
-    def postTweet(self, user_id, tweet_text):
-        # write your code here
-        if user_id not in self.message_dict:
-            self.message_dict[user_id] = []
-        if len(self.message_dict[user_id]) == 10:
-            self.message_dict[user_id].pop(0)
-            
-        tweet = Tweet(user_id, tweet_text)
-        self.message_dict[user_id].append((self.time, tweet))
-        self.time += 1 
-        return tweet
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.user_tweets:
+            self.user_tweets[userId] = []
+        self.user_tweets[userId].append((self.orderId, tweetId))
+        self.orderId += 1
 
-    """
-    @param: user_id: An integer
-    @return: a list of 10 new feeds recently and sort by timeline
-    """
-    def getNewsFeed(self, user_id):
-        # write your code here
-        to_user_ids = self.follow_dict.get(user_id, set())
-        # |    &
-        user_ids = to_user_ids | set([user_id])
-        messages = []
-        for user in user_ids:
-            messages.extend(self.message_dict.get(user, []))
-        print(user_ids, )
-            
-        messages = sorted(messages, key = lambda x: -x[0])
-        return [_[1] for _ in messages[:10]]
+    def getNewsFeed(self, userId: int) -> List[int]:
+        # fetch the last 10 tweets for the userId itself + followed users
+        # input the (orderId, tweetId) to the minheap, and keep the minheap number to 10
+        # then return result with order
+        # Time complexity: O(F* 10 log 10), F - max followed users
+        heap = []
+        users = set([userId])
+        users.update(self.followed_users.get(userId, set()))
+
+        for user in users:
+            tws = self.user_tweets.get(user, [])
+            for tw in tws[-10:]:
+                heapq.heappush(heap, tw)
+                if len(heap) > 10:
+                    heapq.heappop(heap)
+
+        result = []
+        while heap:
+            # from the small orderId (least recent) -> recent
+            result.append(heapq.heappop(heap)[1])
+
+        return result[::-1]
         
 
-    """
-    @param: user_id: An integer
-    @return: a list of 10 new posts recently and sort by timeline
-    """
-    def getTimeline(self, user_id):
-        # write your code here
-        return [_[1] for _ in self.message_dict.get(user_id, [])][::-1]
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.followed_users:
+            self.followed_users[followerId] = set([])
 
-    """
-    @param: from_user_id: An integer
-    @param: to_user_id: An integer
-    @return: nothing
-    """
-    def follow(self, from_user_id, to_user_id):
-        # write your code here
-        if from_user_id not in self.follow_dict:
-            self.follow_dict[from_user_id] = set()
-            
-        self.follow_dict[from_user_id].add(to_user_id)
+        self.followed_users[followerId].add(followeeId)
+        
 
-    """
-    @param: from_user_id: An integer
-    @param: to_user_id: An integer
-    @return: nothing
-    """
-    def unfollow(self, from_user_id, to_user_id):
-        # write your code here
-        if to_user_id in self.follow_dict.get(from_user_id, set()):
-            self.follow_dict[from_user_id].remove(to_user_id)
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followed_users:
+            # unlike remove, discard does not raise error
+            self.followed_users[followerId].discard(followeeId)
+        
+
+####    max heap #####
+
+import heapq
+from collections import defaultdict
+
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.followed = defaultdict(set)
+        self.tweets = defaultdict(list)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append((self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+
+        users = self.followed[userId] | {userId}
+
+        heap = []
+
+        # push latest tweet of each user
+        for u in users:
+            if self.tweets[u]:
+                idx = len(self.tweets[u]) - 1
+                time, tweetId = self.tweets[u][idx]
+
+                heapq.heappush(heap, (-time, tweetId, u, idx - 1))
+
+        res = []
+
+        while heap and len(res) < 10:
+            time, tweetId, user, idx = heapq.heappop(heap)
+            res.append(tweetId)
+
+            # push next tweet from the same user
+            if idx >= 0:
+                time, tweetId = self.tweets[user][idx]
+                heapq.heappush(heap, (-time, tweetId, user, idx - 1))
+
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.followed[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followed[followerId].discard(followeeId)
