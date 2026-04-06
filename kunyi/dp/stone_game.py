c848199f4de876ac0e12c128938e6b94839cd480
Kunyi Liu <kl2912@columbia.edu>
2026-04-05 22:15:17 -0700

    Implement stoneGame method in Solution class

---
+++ kunyi/dp/stone_game.py
@@ -0,0 +1,5 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        # Both players play optimally -> not playing in a greedy way.
+        # with each number of piles and total odd number, there has to be a winner. So the first player can win.
+        return True
