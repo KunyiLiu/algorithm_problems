a1da712a3b77fe127a92a68c58ec991e3183866f
Kunyi Liu <kl2912@columbia.edu>
2026-05-16 23:59:42 -0700

    Enhance calculator with nested expression support
    Refactor the basic calculator to support nested expressions using recursion. Introduced a helper function to handle calculations within parentheses.

--- kunyi/data_structure_ii/stack/basic-calculator-iii.py
+++ kunyi/data_structure_ii/stack/basic-calculator-iii.py
@@ -43,3 +43,55 @@ class Solution:
             self.ind += 1 
             
         return sum(stack)
+
+# nested function
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.strip()
+        global_start = 0
+        
+        # create a recurssion + stack. helper(start, s) -> return result; new stack every time hitting (
+        def helper(s):
+            nonlocal global_start
+            stack = []
+            num, sign = 0, "+"
+
+            while global_start < len(s) and s[global_start] != ")":
+                if s[global_start].isspace():
+                    global_start += 1
+                    continue 
+
+                if s[global_start] == "(":
+                    global_start += 1
+                    num = helper(s)
+                else:
+                    num = 0
+                    while global_start < len(s) and s[global_start].isdigit():
+                        num = num * 10 + int(s[global_start])
+                        global_start += 1
+
+                if sign == "+":
+                    stack.append(num)
+                elif sign == "-":
+                    stack.append(-num)
+                elif sign == "*":
+                    last = stack.pop()
+                    stack.append(last * num)
+                elif sign == "/":
+                    last = stack.pop()
+                    stack.append(int(last / num))
+                
+                if global_start >= len(s):
+                    break
+
+                if s[global_start] == ")":
+                    global_start += 1
+                    return sum(stack)
+
+                sign = s[global_start]
+                global_start += 1
+
+            return sum(stack)
+
+        return helper(s)
